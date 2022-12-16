67bf8a75da2ef39190601a1ffaae6d9f7803df2a
technomancy7 <kylebell68@gmail.com>
2022-12-16 13:29:21 +0000

    Sync

--- __main__.py
+++ __main__.py
@@ -5,48 +5,56 @@ import pygame
 import json
 import objects
 
+
+
 # define a main function
-def main():
-    cfg = {}
-    with open("config.json", "r+") as f:
-        cfg = json.load(f)
+class Main:
+    def __init__(self):
+        self.cfg = {}
+        with open("config.json", "r+") as f:
+            self.cfg = json.load(f)
+            
+        # initialize the pygame module
+        pygame.init()
+
+        # load and set the logo
+        #logo = pygame.image.load("logo32x32.png")
+        #pygame.display.set_icon(logo)
+        pygame.display.set_caption("WIP")
+
+        # create a surface on screen
+        self.screen = pygame.display.set_mode(self.cfg["winsize"])
+
+        # define a variable to control the main loop
+        self.running = True
+
+
+        # State values
+        self.variables = {}
+        self.entities = []
+        self.zones = []
         
-    # initialize the pygame module
-    pygame.init()
-    
-    # load and set the logo
-    #logo = pygame.image.load("logo32x32.png")
-    #pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
-    
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode(cfg["winsize"])
-    
-    # define a variable to control the main loop
-    running = True
-    
-    
-    i = 0
-    
-    # main loop
-    while running:
-        # event handling, gets all event from the eventqueue
-        for event in pygame.event.get():
-            # only do something if the event if of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
-    
-        # TECHNO: test code to see if display is working
-        i += 1
-        if i >= 256: i = 0
-        screen.fill((i,0,0))
-        
-        
-        pygame.display.flip()
-    
+    def loop(self):
+
+        # main loop
+        while self.running:
+            # event handling, gets all event from the eventqueue
+            for event in pygame.event.get():
+                # only do something if the event if of type QUIT
+                if event.type == pygame.QUIT:
+                    # change the value to False, to exit the main loop
+                    self.running = False
+
+            # TECHNO: test code to see if display is working
+            self.screen.fill((0,0,0))
+            
+            
+            # Updates the display
+            pygame.display.flip()
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
     # call the main function
-    main()
+    runtime = Main()
+    runtime.loop()
--- objects.py
+++ objects.py
@@ -1,14 +1,27 @@
  
-
+# Base class of all in-world objects
 class TObject:
     pass
 
-class Entity(TObject):
+class Zone(TObject):
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.sprite = None
-        self.hidden = False
+        self.contains = [] # entities inside this zone
+        
+# Entity objects, characters, NPC's, decorations, etc'
+class Entity(TObject):
+    def __init__(self, ID):
+        self.ID = ID
+        self.x = 0 # X position on screen
+        self.y = 0 # Y position on screen
+        self.sprite = None # Display image
+        self.hidden = False # Wether it's being rendered'
+        self.zone = None # Zone the entity is inside
+        self.contains = [] # Character or containers inventory
+    
+    def move_pos(self, x, y):
+        self.x = x
+        self.y = y
 
+# The Player!
 class Player(Entity):
     pass
