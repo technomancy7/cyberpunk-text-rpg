cd9078272ffbf3c583c8c20817a267863458962d
technomancy7 <kylebell68@gmail.com>
2022-12-26 17:49:50 +0000

    Sync

--- install_linux.sh
+++ install_linux.sh
@@ -1,2 +1,4 @@
 CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$CURRENT_FILE_DIR/py3.9/bin/pypy -m pip install pygame --target $CURRENT_FILE_DIR/libraries/
+export LOCAL_BASE="$CURRENT_FILE_DIR/sdl_x64_linux/" 
+export SDL_CONFIG="$CURRENT_FILE_DIR/sdl_x64_linux/sdl2-config"
+$CURRENT_FILE_DIR/py3.9/bin/pypy -m pip install pygame==2.1.2 --target $CURRENT_FILE_DIR/libraries/
