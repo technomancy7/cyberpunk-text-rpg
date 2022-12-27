a45e0ad7954baae0b73ea6708204ba1ab001e67c
technomancy7 <kylebell68@gmail.com>
2022-12-27 10:51:30 +0000

    Testing windows distribution

---
+++ run_win64.bat
@@ -0,0 +1,6 @@
+SET mypath=%~dp0
+echo Currently running in %mypath:~0,-1% 
+echo Python runtime is at %mypath:~0,-1%/winpy64/python-3.10.8.amd64/python.exe
+echo Testing...
+%mypath:~0,-1%/winpy64/python-3.10.8.amd64/python.exe --version
+%mypath:~0,-1%/winpy64/python-3.10.8.amd64/python.exe %mypath:~0,-1%
